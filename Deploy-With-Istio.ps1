daddfed0e187d7598d2686cddda09cda2a3e20eb
leekester <58551952+leekester@users.noreply.github.com>
2023-05-11 17:11:09 +0100

    Create Deploy-With-Istio.ps1

---
+++ Deploy-With-Istio.ps1
@@ -0,0 +1,43 @@
+$resourceGroup = "rg-aks"
+$location = "uksouth"
+$clusterName = "aks"
+$nodesize = "Standard_B4ms"
+$nodeCount = "1"
+$subscriptionId = (az account show --query id --output tsv)
+
+# Create AKS cluster
+Write-Host "Creating AKS cluster $clusterName in resource group $resourceGroup" -ForegroundColor Yellow
+az group create `
+  --name $resourceGroup `
+  --location $location
+az aks create `
+  --resource-group $resourceGroup `
+  --name $clusterName `
+  --node-vm-size $nodesize `
+  --node-count $nodeCount `
+  --network-plugin kubenet `
+  --pod-cidr 192.168.0.0/16 `
+  --zones 1 `
+  --generate-ssh-keys
+
+# Register the service mesh feature and wait until registration completes
+$startTime = (Get-Date)
+Do {
+    $serviceMeshPreviewState = (az feature show --namespace "Microsoft.ContainerService" --name "AzureServiceMeshPreview" | ConvertFrom-Json).properties.state
+    If ($serviceMeshPreviewState -eq "Not Registered") {
+        az feature register --namespace "Microsoft.ContainerService" --name "AzureServiceMeshPreview"
+    }
+    $elapsedSeconds = ((Get-Date) - $startTime).Seconds
+    Write-Host ("Feature is in a `"" + $serviceMeshPreviewState + "`" state. Registration started " + $elapsedSeconds + " seconds ago.") -ForegroundColor Yellow
+    Sleep 10
+} Until ($serviceMeshPreviewState -eq "Registered")
+
+# Register/re-register the provider
+Write-Host "Registering/re-registering the Microsoft.ContainerService provider" -ForegroundColor Yellow
+az provider register --namespace Microsoft.ContainerService
+
+az aks mesh enable --resource-group ${RESOURCE_GROUP} --name ${CLUSTER}
+
+# Retrieve AKS admin credentials
+Write-Host "Retrieving AKS credentials" -ForegroundColor Yellow
+az aks get-credentials --name $clusterName --resource-group $resourceGroup --overwrite-existing
